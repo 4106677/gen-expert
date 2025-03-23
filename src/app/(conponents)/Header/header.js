320d7d7762b46937e6eca0592979f3b05cc24fae
Yaroslav <4106677@gmail.com>
2025-03-23 15:13:25 +0200

    fix link

--- src/app/(conponents)/Header/header.js
+++ src/app/(conponents)/Header/header.js
@@ -1,6 +1,7 @@
 // src/app/components/Header/header.js
 import './header.css';
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
 	return (
@@ -8,10 +9,10 @@ export default function Header() {
 			<nav className="nav container">
 				<Image height={50} width={50} src="/images/logo.svg" alt="Logo" className="logo" />
 				<ul className="nav-list">
-					<li><a href="/">Головна</a></li>
-					<li><a href="/products">Товари</a></li>
-					<li><a href="/contacts">Контакти</a></li>
-					<li><a href="/calculator">Калькулятор</a></li>
+					<li><Link href="/">Головна</Link></li>
+					<li><Link href="/products">Товари</Link></li>
+					<li><Link href="/contacts">Контакти</Link></li>
+					<li><Link href="/calculator">Калькулятор</Link></li>
 				</ul>
 			</nav>
 		</header>
