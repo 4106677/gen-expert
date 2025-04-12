99e51bddb26002044b50b32710bb6d25e0296cf6
Yaroslav <4106677@gmail.com>
2025-04-12 12:46:20 +0300

    Update GoogleTagManagerNoScript.jsx

--- src/app/(components)/GoogleTagManagerNoScript.jsx
+++ src/app/(components)/GoogleTagManagerNoScript.jsx
@@ -1,8 +1,12 @@
 export default function GoogleTagManagerNoScript() {
 	return (
 		<noscript>
-			<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WQQ2LK24"
-			        height="0" width="0" style="display:none;visibility:hidden"></iframe>
+			<iframe
+				src="https://www.googletagmanager.com/ns.html?id=GTM-WQQ2LK24"
+				height="0"
+				width="0"
+				style={{ display: 'none', visibility: 'hidden' }}
+			/>
 		</noscript>
 	);
 }
